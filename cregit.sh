fa87cc631e3179509b048363c77ac37b27b07f67
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-07-12 15:41:49 +0200

    lukaszgryglicki-new-tools

---
+++ cregit.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+VERSION=2.13
+PRODUCT=git
+
+CREATE_TOKEN_REPO=no
+CREATE_PERSONS=no
+CREATE_DBS=no
+CREATE_BLAME=no
+CREATE_HTML=yes
+
+CREGIT=/home/dmg/git.dmg/cregit-scala
+
+CREGITDB=token.db
+ORIGINALDB=line.db
+PERSONSDB=persons.db
+
+TOKENREPO=token
+ORIGINALREPO=../original.repo/git
+
+export BFG_MEMO_DIR=`pwd`/memo
+BLAME_DIR=`pwd`/blame
+EXTENSION='\.[ch]$'
+HTML_DIR=`pwd`/html
+COMMIT_URL=https://github.com/git/git/commit/
+
+error_exit()
+{
+
+#   ----------------------------------------------------------------
+#   Function for exit due to fatal program error
+#       Accepts 1 argument:
+#           string containing descriptive error message
+#   ---------------------------------------------------------------- 
+
+    echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+    exit 1
+}
+
+
+if [ ! -d "$ORIGINALREPO" ]; then
+    # Control will enter here if $DIRECTORY doesn't exist.
+    error_exit "Error! original repo does not exist [$ORIGINALREPO]" 
+fi
+
+if [ ! -d "$TOKENREPO" ]; then
+    # Control will enter here if $DIRECTORY doesn't exist.
+    error_exit "Error! token repo does not exist [$TOKENREPO]" 
+fi
+
+if [ "$CREATE_TOKEN_REPO" = "yes" ]; then 
+    
+    if [ ! -d "$BFG_MEMO_DIR" ]; then
+        # Control will enter here if $DIRECTORY doesn't exist.
+        mkdir $BFG_MEMO_DIR || error_exit
+    fi
+
+    cd $TOKENREPO || error_exit "Error! token repo does not exist" 
+
+    export BFG_TOKENIZE_CMD='/home/dmg/git.dmg/cregit-scala/tokenize/tokenizeSrcMl.pl --srcml2token=/home/dmg/git.dmg/cregit-scala/tokenize/srcMLtoken/srcml2token --srcml=srcml --ctags=ctags-exuberant'
+
+    java -jar ${CREGIT}/jars/bfg.jar '--blob-exec:/home/dmg/git.dmg/cregit-scala/tokenizeByBlobId/tokenBySha.pl=\.[ch]$' --no-blob-protection
+
+    git reset --hard
+    git reflog expire --expire=now --all && git gc --prune=now --aggressive
+
+    cd ..
+fi
+if [ "$CREATE_DBS" = "yes" ]; then 
+  java -jar ${CREGIT}/jars/slickGitLog.jar $CREGITDB $TOKENREPO
+  java -jar ${CREGIT}/jars/slickGitLog.jar $ORIGINALDB $ORIGINALREPO
+  java -jar ${CREGIT}/jars/remapcommits.jar $CREGITDB $TOKENREPO
+fi
+
+if [ "$CREATE_PERSONS" = "yes" ]; then 
+   java -jar ${CREGIT}/jars/persons.jar $TOKENREPO persons.xls $PERSONSDB
+
+   printf "drop table persons;\n" | sqlite3 $PERSONSDB
+   printf "create table persons(personid text primary key not null, personname text);\n" | sqlite3 $PERSONSDB
+   printf "insert into persons(personid) select distinct personid from emails;\n" | sqlite3 $PERSONSDB
+
+fi
+
+if [ "$CREATE_BLAME" = "yes" ]; then 
+    if [ ! -d "$BLAME_DIR" ]; then
+        # Control will enter here if $DIRECTORY doesn't exist.
+        mkdir $BLAME_DIR || error_exit
+    fi
+
+   perl ${CREGIT}/blameRepo/blameRepoFiles.pl --verbose $TOKENREPO $BLAME_DIR $EXTENSION
+fi
+
+if [ "$CREATE_HTML" = "yes" ]; then 
+    if [ ! -d "$HTML_DIR" ]; then
+        # Control will enter here if $DIRECTORY doesn't exist.
+        mkdir $HTML_DIR || error_exit
+    fi
+
+    perl ${CREGIT}/prettyPrint/prettyPrintFiles.pl --verbose $CREGITDB $PERSONSDB $ORIGINALREPO $BLAME_DIR $HTML_DIR $COMMIT_URL $EXTENSION
+fi
---
+++ tokenize/kuber-tokenize.pl
@@ -0,0 +1,276 @@
+#!/usr/bin/perl
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use File::Basename;
+
+
+my %declarations;
+my %listDeclarations;
+
+my %languages = ("Go" => 1,
+                 "Markdown" => 1,
+                 "Yaml" => 1);
+
+use Getopt::Long;
+
+my $usage = "
+Usage $0 [options] <sourcefilename> <outputfile>*
+        
+Options:
+   --language=<Go/Markdown/Yaml>
+   --ctags=-<path to ctags-exuberant>
+";
+
+
+my $basedir = dirname($0);
+$basedir = "." if ($basedir eq "");
+
+my $go2token = "$basedir/goTokenizer/gotoken";
+my $simpleTokenizer = "$basedir/text/simpleTokenizer.pl";
+my $language = "Go";
+my $ctags = "ctags-exuberant";
+
+my $verbose;
+GetOptions (
+            "go2token=s"   => \$go2token,
+            "language=s"      => \$language,
+            "ctags=s"         => \$ctags,
+            "simpleTokenizer=s" => \$simpleTokenizer,
+            "verbose"  => \$verbose)   # flag
+  or die($usage);
+
+if (not defined($languages{$language})) {
+    die($usage);
+}
+
+
+
+my $filename = shift;
+my $output = shift;
+
+die $usage if $filename eq "";
+
+print STDERR "Tokenizing $filename\n";
+if ($output ne "") {
+    open(OUT, ">$output") or die "Unable to create output file\n";
+    select OUT;
+}
+
+if ($language eq "Go") {
+
+    Read_Declarations($filename, $language);
+
+    #Declarations_Test();
+
+    Tokenize($filename);
+
+    if ($output ne "") {
+        close(OUT);
+    }
+} else {
+    open(parser, "$simpleTokenizer '$filename' |") or die "Unable to execute [$go2token] on file [$filename]";
+    print "begin_unit\n";
+    while(<parser>) {
+        print("text|" . $_);
+    }
+    close(parser);
+    print "end_unit\n";
+}
+
+exit;
+
+
+sub Tokenize
+{
+    my $saveDir = `pwd`;
+    chomp $saveDir;
+    #    my $PARSER = "srcml --src-encoding utf8 --position '$filename' | tokenizeSrcML adf | ";
+    my ($filename) = @_;
+    #    open(parser, "srcml --src-encoding utf8 -l C --position '$filename' | srcml2token |") or die "Unable to execute ctags on file [$filename]";
+    open(parser, "$go2token '$filename' |") or die "Unable to execute [$go2token] on file [$filename]";
+
+    my $lastLine = -1;
+
+    while (<parser>) {
+        #        print STDERR;
+        chomp;
+        my $line =$_;
+        die "unable to parse srcml line [$line]" unless $line =~ /^([0-9]+|-):([0-9]+|-)\s+(.+)$/;
+        my ($line, $col, $token) = ($1, $2, $3);
+        $token = Remap_Token($token);
+#        print STDERR "$line:$col:[$token]\n";
+        die "ilegall line [$line] with [$line][$col]" if $line eq '' ;
+        if ($line != $lastLine) {
+            my @d = Declarations_In_Line($line);
+            foreach my $dec (@d) {
+                my %thisDec = Get_Declaration($line, $dec);
+                print "DECL|";
+                print "$thisDec{type}|$thisDec{name}\n";
+            }
+        } 
+        $lastLine = $line;
+        print "$token\n";
+        if ($token =~ /^end_/) {
+            printf "\n";
+        }
+
+    }
+    close parser;
+    chdir($saveDir);
+}
+
+
+
+sub Declarations_Test
+{
+    foreach my $line (sort {$a <=> $b} keys %declarations) {
+        # each element is an array of hashes
+        print "Line: $line\n";
+        #    my $t = $decls{$line}{name};
+        #    my %h = @$t;
+        #    foreach my $k2 (sort keys %h) {
+        #        print "  $k2 => $h{$k2}\n";
+        #    }
+        print "\n";
+        print "Test decl in line [$line]\n";
+        print join(':', Declarations_In_Line($line));
+        print "\n";
+        next;
+        print "Test Get_Declarations [$line]\n";
+        my @d = Declarations_In_Line($line);
+        foreach my $dec (@d) {
+            print "Declarations for [$line][$dec]\n";
+            
+            my %thisDec = Get_Declaration($line, $dec);
+            foreach my $k (sort keys %thisDec) {
+                print "   $k => $thisDec{$k}\n";
+            }
+            print "\n";
+        }
+        print "End Test Get_Declarations [$line]\n";
+        print "\n";
+    }
+}
+
+sub Declarations_In_Line
+{
+    my ($line) = @_;;
+    my $a = $listDeclarations{$line};
+    return () if not defined($a);
+    return (@$a);
+}
+
+sub Get_Declaration
+{
+    my ($line, $name) = @_;
+    my $d = $declarations{$line}{$name};
+    die "Illegal value in get declaration [$line][$name]" unless defined $d;
+    my %h = @$d;
+    return %h;
+}
+
+
+sub Read_Declarations
+{
+    my ($filename, $language) = @_;
+    my $CTAGS = "$ctags --language-force=$language -x -u";
+
+    open(ctags, "$CTAGS '$filename'|") or die "Unable to execute ctags on file [$filename]";
+
+    while (<ctags>) {
+        my %decl;
+        my $rest;
+        my $line;
+        chomp;
+        $decl{original} = $_;
+        die "unable to parse output" unless /^([^ ]+)\s+([^ ]+)\s+([0-9]+)\s+(.+)$/;
+        ($decl{name}, $decl{type}, $decl{line}, $rest) = ($1, $2, $3, $4);
+        $line = $decl{line};
+        # skip the filename
+        $decl{decl} = substr($rest, length($filename)+1);
+        $decl{decl} =~ s/^\s+//;
+        # we might have more than one declaration per line
+        $declarations{$line}{$decl{name}} = [%decl];
+        if (not defined $listDeclarations{$line}) {
+            $listDeclarations{$line} = [];
+        }
+        my $v = $listDeclarations{$line};
+        push (@$v, $decl{name});
+        # build the return data structure
+        # a hash of the four values
+    }
+    close ctags;
+}
+
+
+sub Remap_Token {
+    my ($token) = @_;
+
+    if ($token eq "||") {
+        return "barbar|||"
+    }
+
+    if ($token =~ /^(.+)\|(.+)$/) {
+        my ($left, $right) = ($1, $2);
+        return $left . "|" . Unquote($right, $left);
+    } elsif ($token eq "begin_unit" or $token eq "end_unit") {
+        # these tokens should have no text
+        return $token;
+    } else {
+        return ($token . "|" . $token);
+    }
+
+}
+
+sub Unquote {
+    my ($token, $type) = @_;
+
+    if ($type eq "STRING" or
+        $type eq "COMMENT" 
+       ) {
+        $token =~ s/^"(.+)"$/$1/;
+        $token =~ s/\\"/"/g;
+        $token =~ s/\\r/ /g;
+        $token =~ s/\\t/ /g;
+        $token =~ s/\\n/ /g;
+        $token =~ s/\\\\u/\\u/g;
+        $token =~ s/\\\\x/\\x/g;
+
+        # this must be the very last one
+        $token =~ s/\\\\/\\/g;
+    } elsif ($type eq "CHAR") {
+        $token =~ s/^"(.+)"$/$1/;
+        $token =~ s/\\"/"/g;
+        $token =~ s/\\\\/\\/g;
+    } elsif ($type eq ";") {
+        if ($token eq '"\n"') {
+            $token= "";
+        } else {
+            $token =~ s/^"(.+)"$/$1/;
+        }
+    } elsif ($type eq "IDENT") {
+        $token =~ s/^"(.+)"$/$1/;
+    } elsif ($type eq "INT") {
+        $token =~ s/^"(.+)"$/$1/;
+    } elsif ($token ne "") {
+        $token =~ s/^"(.+)"$/$1/;
+    } else {
+    }
+
+    return $token;
+
+
+}
