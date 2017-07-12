29612c1b23d920b0c94005c87850aef4c89fc4ff
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-07-12 09:47:28 +0200

    lukaszgryglicki-attempt-1st-sumbcommand-run

---
+++ run_tokenize.sh
@@ -0,0 +1,3 @@
+export BFG_MEMO_DIR=/tmp/memo
+export BFG_TOKENIZE_CMD=/home/justa/dev/cregit/tokenize/tokenizeSrcMl.pl --srcml2token=srcml2token --srcml=srcml --ctags=ctags-exuberant
+java -jar bfg-cregit.jar '--blob-exec:/home/justa/dev/cregit/tokenizeByBlobId/tokenBySha.pl=.[ch]$' --no-blob-protection /path/repo
