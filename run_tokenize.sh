d8d8adbcfd90e02a7102259661c86e27eeefd398
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-07-12 10:17:31 +0200

    lukaszgryglicki-before-debug-error

--- run_tokenize.sh
+++ run_tokenize.sh
@@ -1,3 +1,3 @@
-export BFG_MEMO_DIR=/tmp/memo
-export BFG_TOKENIZE_CMD=/home/justa/dev/cregit/tokenize/tokenizeSrcMl.pl --srcml2token=srcml2token --srcml=srcml --ctags=ctags-exuberant
-java -jar bfg-cregit.jar '--blob-exec:/home/justa/dev/cregit/tokenizeByBlobId/tokenBySha.pl=.[ch]$' --no-blob-protection /path/repo
+BFG_MEMO_DIR=/tmp/memo
+BFG_TOKENIZE_CMD='/home/justa/dev/cregit/tokenize/tokenizeSrcMl.pl --srcml2token=srcml2token --srcml=srcml --ctags=ctags-exuberant'
+java -jar bfg-cregit.jar '--blob-exec:/home/justa/dev/cregit/tokenizeByBlobId/tokenBySha.pl=.[ch]$' --no-blob-protection /home/justa/dev/kubernetes
