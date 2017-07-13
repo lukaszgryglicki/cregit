8e597ef47d6a387f494d090b2208b716475a66cc
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2017-07-13 15:43:29 +0000

    lukaszgryglicki-from-within-packet-type-2-java-mem-limits

--- run_tokenize.sh
+++ run_tokenize.sh
@@ -1,5 +1,6 @@
 BFG_MEMO_DIR=/tmp/memo; export BFG_MEMO_DIR
 BFG_TOKENIZE_CMD='/home/justa/dev/cregit/tokenize/tokenizeSrcMl.pl --go2token=/home/justa/dev/cregit/tokenize/goTokenizer/gotoken --simpleTokenizer=/home/justa/dev/cregit/tokenize/text/simpleTokenizer.pl --srcml2token=srcml2token --srcml=srcml --ctags=ctags-exuberant'; export BFG_TOKENIZE_CMD
+SBT_OPTS='-Xms100g -Xmx100g -XX:ReservedCodeCacheSize=2048m -XX:MaxMetaspaceSize=25g'; export SBT_OPTS
 cd ../bfg-repo-cleaner/
 rm perllog.txt
 sbt 'bfg/run --blob-exec:/home/justa/dev/cregit/tokenizeByBlobId/tokenBySha.pl=\.([ch]|go|md|sh|yaml)$ --no-blob-protection /home/justa/dev/kubernetes'
